914d528ef8bbf422e2ae5fa9925a06c1df3f0f76
pravakar15031986 <68266118+pravakar15031986@users.noreply.github.com>
2023-10-16 10:33:04 +0530

    finalsprintdemo

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Welcome to CSM Technologies pvt. in Azure DevOps automation Platform we recommended to use these services like AKS, Best Technology which i have worked !";
+		return "Welcome to CSM Technologies pvt. in Azure DevOps automation Platform And Binaya Mahapatra is expert in Azure platform , Best Technology which i have worked !";
 	}
 
 	public static void main(String[] args) {

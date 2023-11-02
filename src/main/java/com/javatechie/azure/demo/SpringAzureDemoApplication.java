425f1e0da1aeb9a6a8966f48743971546d578ba8
pravakar15031986 <68266118+pravakar15031986@users.noreply.github.com>
2023-11-02 10:47:54 +0530

    extraordinary commit

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "WELCOME TO CSM TECHNOLOGIES IN Azure DevOps Platform, Binaya Mohapatra, Pravakar kumar And Bren Sahu is expert in Azure platform please check!";
+		return "WELCOME TO CSM TECHNOLOGIES IN Azure DevOps Platform, Binaya Mohapatra, Pravakar kumar And Bren Sahu is expert in Azure platform !";
 	}
 
 	public static void main(String[] args) {

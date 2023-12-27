dad70d3250ea6daa21a2871b46f8882db0bc2c2a
pravakar15031986 <68266118+pravakar15031986@users.noreply.github.com>
2023-12-27 21:09:34 +0530

    update

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "WELCOME TO CSM TECHNOLOGIES IN Azure DevOps Platform, Binaya Mohapatra, Pravakar kumar And Bren Sahu is expert in Azure platform WELCOME we are going to cloud!";
+		return "WELCOME TO Anup TECHNOLOGIES IN Azure DevOps Platform, Binaya Mohapatra, Pravakar kumar And Bren Sahu is expert in Azure platform WELCOME we are going to cloud!";
 	}
 
 	public static void main(String[] args) {
